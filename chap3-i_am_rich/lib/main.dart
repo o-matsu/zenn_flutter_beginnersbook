bf1b199521400ddc161b22e584b5cff7e21870f1
o_matsu555 <gggggiiiiiiii@gmail.com>
2020-11-14 10:51:13 +0900

    step.2 scaffold, NetworkImage

--- chap3-i_am_rich/lib/main.dart
+++ chap3-i_am_rich/lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
+// the main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World!'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://images-na.ssl-images-amazon.com/images/I/71X792rclDL.png'),
+          ),
+        ),
       ),
     ),
   );
