15b764254829fa8b7b50f9490f76ac8784b71fe1
o_matsu555 <gggggiiiiiiii@gmail.com>
2020-12-12 12:43:55 +0900

    rename projects.

--- chapter05_MiCard/README.md
+++ chapter05_MiCard/README.md
@@ -1,11 +1,12 @@
 # chapter.5 画面レイアウトを学ぼう ~ Mi Card ~
 
 ## このチャプターで学べること
-1. HotReload / HotRestart の違いと使い方
-2. レイアウトによく利用するウィジェットの使い方
+1. レイアウトによく利用するウィジェットの使い方
     - Container
     - Column / Row
     - Card
+2. HotReload / HotRestart の違いと使い方
 
 ## ScreenShots
 ![ss-chapter05-01](https://user-images.githubusercontent.com/7794917/101270097-170ae700-37b9-11eb-9eda-226c833b06a6.png)
+<img src="https://user-images.githubusercontent.com/7794917/101270097-170ae700-37b9-11eb-9eda-226c833b06a6.png" width="100">
--- chapter05_MiCard/lib/main.dart
+++ chapter05_MiCard/lib/main.dart
@@ -3,92 +3,91 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); // Statelessウィジェットの呼び出し
 }
 
+// Statelessウィジェットを定義
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.white,
-                backgroundImage: AssetImage("images/face.jpg"),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage("images/face.jpg"),
+            ),
+            Text(
+              "O-matsu",
+              style: TextStyle(
+                fontFamily: "Pacifico",
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-              Text(
-                "O-matsu",
-                style: TextStyle(
-                  fontFamily: "Pacifico",
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                fontFamily: "SourceSansPro",
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal[50],
               ),
-              Text(
-                "FLUTTER DEVELOPER",
-                style: TextStyle(
-                  fontFamily: "SourceSansPro",
-                  fontSize: 20.0,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal[50],
-                ),
+            ),
+            SizedBox(
+              height: 30.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal[100],
               ),
-              SizedBox(
-                height: 30.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal[100],
-                ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 50,
               ),
-              Card(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 50,
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
                 ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    "+81 999 9999",
-                    style: TextStyle(
-                      fontFamily: "SourceSansPro",
-                      fontSize: 20.0,
-                      color: Colors.teal[900],
-                    ),
+                title: Text(
+                  "+81 999 9999",
+                  style: TextStyle(
+                    fontFamily: "SourceSansPro",
+                    fontSize: 20.0,
+                    color: Colors.teal[900],
                   ),
                 ),
               ),
-              Card(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 50,
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 50,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
                 ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    "sample.mail@gmail.com",
-                    style: TextStyle(
-                      fontFamily: "SourceSansPro",
-                      fontSize: 20.0,
-                      color: Colors.teal[900],
-                    ),
+                title: Text(
+                  "sample.mail@gmail.com",
+                  style: TextStyle(
+                    fontFamily: "SourceSansPro",
+                    fontSize: 20.0,
+                    color: Colors.teal[900],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
